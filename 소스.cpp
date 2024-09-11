13b0c68e2df46ed23b4cb6d928834a057453feff
RcoasteR0 <st_anthony@naver.com>
2024-09-11 16:57:07 +0900

    initialize

---
+++ 소스.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <random>
+#include <math.h>
+#include <Windows.h>
+
+#define Quiz4
+
+using namespace std;
+
+random_device rd;
+mt19937 gen(rd());
+
+int main()
+{
+
+}
