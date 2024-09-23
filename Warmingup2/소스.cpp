a45ae70163f4a522195fa48cd7d483a5a102126e
RcoasteR0 <92726320+RcoasteR0@users.noreply.github.com>
2024-09-24 01:33:56 +0900

    문제4

--- Warmingup2/소스.cpp
+++ Warmingup2/소스.cpp
@@ -21,13 +21,36 @@ struct Card
 	bool exclude = false;
 };
 
+void DisplayBoard(Card card[][5])
+{
+	cout << "  a b c d e " << endl;
+	for (int i = 0; i < 5; ++i)
+	{
+		cout << i + 1 << " ";
+
+		for (int j = 0; j < 5; ++j)
+		{
+			if (card[i][j].exclude)
+			{
+				if(card[i][j].type != '@')
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), int(card[i][j].type - 'A' + 1));
+				cout << card[i][j].type << " ";
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+			}
+			else
+				cout << "* ";
+		}
+
+		cout << endl;
+	}
+}
 #endif // Quiz4
 
 int main()
 {
 #ifdef Quiz4
 	Card card[5][5];
-	char command;
+	char x1 = ' ', y1, x2, y2;
 
 	uniform_int_distribution<int> randidx(0, 24);
 	for (int i = 0; i < 25; ++i)
@@ -45,13 +68,85 @@ int main()
 			--i;
 	}
 
-	for (int i = 0; i < 5; ++i)
+	while (x1 != 'q')
 	{
-		for (int j = 0; j < 5; ++j)
-		{
-			cout << card[i][j].type << " ";
-		}
+		DisplayBoard(card);
+		cout << "좌표 입력: ";
+		cin >> x1 >> y1 >> x2 >> y2;
 
+		if (x1 == 'r')
+		{
+			for (int i = 0; i < 25; ++i)
+			{
+				card[0][i].type = '\0';
+				card[0][i].exclude = false;
+			}
+
+			for (int i = 0; i < 25; ++i)
+			{
+				int x = randidx(gen);
+
+				if (card[0][x].type == '\0')
+				{
+					if (i == 24)
+						card[0][x].type = '@';
+					else
+						card[0][x].type = 'A' + i / 2;
+				}
+				else
+					--i;
+			}
+		}
+		else if (x1 >= 'a' && x1 <= 'e' && y1 >= '1' && y1 <= '5'
+			&& x2 >= 'a' && x2 <= 'e' && y2 >= '1' && y2 <= '5')
+		{
+			int row1 = x1 - 'a';
+			int col1 = y1 - '1';
+			int row2 = x2 - 'a';
+			int col2 = y2 - '1';
+
+			if (x1 == x2 && y1 == y2)
+				cout << "동일한 좌표를 입력했습니다." << endl;
+			else if (card[col1][row1].exclude || card[col2][row2].exclude)
+				cout << "이미 개방된 카드입니다." << endl;
+			else
+			{
+				card[col1][row1].exclude = true;
+				card[col2][row2].exclude = true;
+
+				DisplayBoard(card);
+
+				if (card[col1][row1].type == '@')
+				{
+					for (int i = 0; i < 25; ++i)
+					{
+						if (card[0][i].type == card[col2][row2].type)
+						{
+							card[0][i].exclude = true;
+							break;
+						}
+					}
+				}
+				else if (card[col2][row2].type == '@')
+				{
+					for (int i = 0; i < 25; ++i)
+					{
+						if (card[0][i].type == card[col1][row1].type)
+						{
+							card[0][i].exclude = true;
+							break;
+						}
+					}
+				}
+				else if (card[col1][row1].type != card[col2][row2].type)
+				{
+					card[col1][row1].exclude = false;
+					card[col2][row2].exclude = false;
+				}
+			}
+		}
+		else if(x1 != 'q')
+			cout << "유효하지 않은 좌표 범위입니다." << endl;
 		cout << endl;
 	}
 
